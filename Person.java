3547bd681e7b6d0316675be941e5df22a7ec6a38
Parthiban Loganathan <pl2487@columbia.edu>
2015-11-20 11:43:40 -0500

    Fix

--- Person.java
+++ Person.java
@@ -11,10 +11,10 @@ public class Person {
     public static enum Status{STRANGER, FRIEND, SOULMATE};
 
     public int id;
-    public int weight = -1;
+    public int wisdom = -1;
     public Status status; // stranger, friend or soul mate
     public Point last_known_position;
     public boolean chatted; // have we chatted with them before
     public int last_seen; // time when we last saw them
-    public int time_spoken; // how long we've spoken with them
+    public int remaining_wisdom; // how long we've spoken with them
 }
--- Player.java
+++ Player.java
@@ -37,7 +37,7 @@ public class Player implements wtr.sim.Player {
             Person friend = people[friend_id];
             friend.id = friend_id;
             friend.status = Person.Status.FRIEND;
-            friend.weight = 50;
+            friend.wisdom = 50;
         }
 	}
 
@@ -47,35 +47,6 @@ public class Player implements wtr.sim.Player {
 	{
 		time++;
 		// find where you are and who you chat with
-        /**
-		int i = 0, j = 0;
-		while (players[i].id != self_id) i++;
-		while (players[j].id != chat_ids[i]) j++;
-		Point self = players[i];
-		Point chat = players[j];
-		// record known wisdom
-		W[chat.id] = more_wisdom;
-		// attempt to continue chatting if there is more wisdom
-		if (wiser) return new Point(0.0, 0.0, chat.id);
-		// try to initiate chat if previously not chatting
-		if (i == j)
-			for (Point p : players) {
-				// skip if no more wisdom to gain
-				if (W[p.id] == 0) continue;
-				// compute squared distance
-				double dx = self.x - p.x;
-				double dy = self.y - p.y;
-				double dd = dx * dx + dy * dy;
-				// start chatting if in range
-				if (dd >= 0.25 && dd <= 4.0)
-					return new Point(0.0, 0.0, p.id);
-			}
-		// return a random move
-		double dir = random.nextDouble() * 2 * Math.PI;
-		double dx = 6 * Math.cos(dir);
-		double dy = 6 * Math.sin(dir);
-		return new Point(dx, dy, self_id);
-         **/
-        return null;
+       return null;
 	}
 }
